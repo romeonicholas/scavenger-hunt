7fed9879fb25b42e5e7ea80b720d770bc5fb760f
romeonicholas <nicholas.romeo@code.berlin>
2024-10-14 11:09:29 +0200

    Refactor app creation and setup logic

--- app/app.py
+++ app/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from . import simple_pages, users
 
-app = Flask(__name__)
-app.config.from_object("app.config")
 
-app.register_blueprint(users.routes.blueprint)
-app.register_blueprint(simple_pages.routes.blueprint)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("app.config")
+
+    register_blueprints(app)
+
+    return app
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(users.routes.blueprint)
+    app.register_blueprint(simple_pages.routes.blueprint)
+
 
 # game in progress page
 # looks for session in progress based on user, displays current step
